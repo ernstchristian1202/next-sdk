c30e5a7e3334bb72231eb5093897a2a4b9790542
Ernst <ernstchristia@gmail.com>
2025-01-09 00:01:16 +0800

    installed @quorini/core & @quorini/ui-react packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { QClient } from "@quorini/core";
+import { QAuth } from "@quorini/ui-react";
+
+QClient.configure({
+  projectId: "67713ed7f97270ea61b74188",
+  env: "development",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <QAuth.Provider>
+      <Component {...pageProps} />
+    </QAuth.Provider>
+  );
 }
